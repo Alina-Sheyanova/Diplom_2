884b838fdaa4d3dfcec111aa1d4c5630468686e1
Alina Sheyanova <sheyanova@proitr.ru>
2023-05-24 16:07:19 +0300

    add info about ingredients

--- src/main/java/constants/ApiEndpoints.java
+++ src/main/java/constants/ApiEndpoints.java
@@ -7,7 +7,7 @@ public class ApiEndpoints {
     public static String LOGIN_USER = "auth/login";
     public static String LOGOUT_USER = "/auth/logout";
     public static String PASSWORD_RESET = "/password-reset";
-    public static String INGREDIENTS_DATA = "/ingredients";
+    public static String INGREDIENTS = "/ingredients";
     public static String CREATE_ORDER = "/orders";
     public static String ALL_ORDERS = "/orders/all";
     public static String UPDATE_USER_AUTH_TOKEN = "/auth/token";
